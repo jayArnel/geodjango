0f328c695a8affbe4ed5c29efd366c12168f515f
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-03-13 00:04:30 +0800

    used GeoModelAdmin for worldborder model in django admin

--- world/admin.py
+++ world/admin.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 
 # Register your models here.
 from models import WorldBorder
 
-admin.site.register(WorldBorder)
+admin.site.register(WorldBorder, admin.GeoModelAdmin)
