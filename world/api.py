016bb57e48eb2cf1c3e5f862f93abecd7e803e8f
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-03-14 21:44:08 +0800

    updated api resource to include geojson attribute

--- world/api.py
+++ world/api.py
@@ -1,8 +1,11 @@
 from tastypie.resources import ModelResource
 from models import WorldBorder
+from tastypie import fields
 
 
 class WorldBorderResource(ModelResource):
+    geojson = fields.CharField(attribute='geojson', readonly=True)
+
     class Meta:
         queryset = WorldBorder.objects.all()
         resource_name = 'worldborder'
